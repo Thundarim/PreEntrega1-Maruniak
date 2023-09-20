eb5ad192968c987bd47b188c18441bc35f7b173f
Lautaro Maruniak <lauti083@gmail.com>
2023-09-20 10:20:22 -0300

    subida al git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
+function App() { 
+   return (
+  <h1>
+    Hola probando la app a ver si funciona esto
+    </h1>
+)
+}
+
+
+export default App
